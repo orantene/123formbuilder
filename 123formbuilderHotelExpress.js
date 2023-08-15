d10a3decb24a3c001de8235024c7829ea79b5597
orantene <orantene@gmail.com>
2023-08-15 03:36:10 -0500

    Update 123formbuilderHotelExpress.js

--- 123formbuilderHotelExpress.js
+++ 123formbuilderHotelExpress.js
@@ -13,3 +13,31 @@ window.addEventListener('load', function(){
 	targetControlInstance.setValue( sourceControlInstance.getValue() );
 });
 })();
+
+
+(function(){
+window.addEventListener('load', function(){
+	var sourceControlId = 108818319, /** THE ID OF THE SENDER CONTROL */
+	targetControlId = 108821457, /** THE ID OF THE RECEIVER CONTROL **/
+
+	domAbstractionLayer = loader.getDOMAbstractionLayer(),
+	sourceControlInstance = loader.getEngine().getDocument().getElementById(sourceControlId);
+
+	sourceControlInstance.on('value-change', function(){
+		domAbstractionLayer.setControlValueById(
+		String(targetControlId),
+		domAbstractionLayer.getControlValueById(
+		String(sourceControlId)
+		)
+		);
+	});
+
+	domAbstractionLayer.setControlValueById(
+	String(targetControlId),
+	domAbstractionLayer.getControlValueById(
+	String(sourceControlId)
+	)
+
+	);
+});
+})();
