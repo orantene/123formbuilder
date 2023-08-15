b238405437cd2929aadd2a4813393feddbda7fda
orantene <orantene@gmail.com>
2023-08-15 04:14:42 -0500

    Update 123formbuilderHotelExpress.js

--- 123formbuilderHotelExpress.js
+++ 123formbuilderHotelExpress.js
@@ -14,3 +14,14 @@ window.onload = joinValues;
 
 
 
+let x = document.getElementById("form");
+x.addEventListener("focus", myFocusFunction, true);
+x.addEventListener("blur", myBlurFunction, true);
+
+function myFocusFunction() {
+  document.getElementById("price-00000082-prefix-acc").style.backgroundColor = "#444444";  
+}
+
+function myBlurFunction() {
+  document.getElementById("price-00000082-prefix-acc").style.backgroundColor = "";  
+}
