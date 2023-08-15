719258cd85e54bd9d89b9bfe71a045a0ce8c9cc4
orantene <orantene@gmail.com>
2023-08-15 05:58:53 -0500

    Update 123formbuilderHotelExpress.js

--- 123formbuilderHotelExpress.js
+++ 123formbuilderHotelExpress.js
@@ -3,7 +3,10 @@ setTimeout(function() {
 	var valueOne = loader.engine.document.getElementById(108817849).getProperty('value.value');
 	var valueTwo = loader.engine.document.getElementById(108817871).getProperty('value.value');
 	var valueThree = loader.engine.document.getElementById(108818319).getProperty('value.value');
-
+    let hotelname = loader.engine.document.getElementById(108818319).getProperty('value.value');
+    document.querySelector("blockquote h5").innerHTML = hotelname;
+    let folionum = loader.engine.document.getElementById(108817871).getProperty('value.value');
+    document.querySelector("blockquote h4").innerHTML = folionum;
 const joinedValues = valueOne + ": " + valueTwo + " - " + valueThree ;
 		   loader.engine.document.getElementById(108821728).setValue(({"value": joinedValues}));
 }, 60);
@@ -12,7 +15,4 @@ const joinedValues = valueOne + ": " + valueTwo + " - " + valueThree ;
 window.onchange = joinValues;
 window.onload = joinValues;
 
-    let hotelname = loader.engine.document.getElementById(108818319).getProperty('value.value');
-    document.querySelector("blockquote h5").innerHTML = hotelname;
-    let folionum = loader.engine.document.getElementById(108817871).getProperty('value.value');
-    document.querySelector("blockquote h4").innerHTML = folionum;
+
