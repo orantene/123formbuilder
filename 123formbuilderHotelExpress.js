00465fc6570e5c9d6e19473c540cf1bea4c67c36
orantene <orantene@gmail.com>
2023-08-15 06:45:21 -0500

    Update 123formbuilderHotelExpress.js

--- 123formbuilderHotelExpress.js
+++ 123formbuilderHotelExpress.js
@@ -3,10 +3,18 @@ setTimeout(function() {
 	var valueOne = loader.engine.document.getElementById(108817849).getProperty('value.value');
 	var valueTwo = loader.engine.document.getElementById(108817871).getProperty('value.value');
 	var valueThree = loader.engine.document.getElementById(108818319).getProperty('value.value');
+
+    let dateDay = document.querySelector("[data-ui-role='ui-element'] #date-00000052-day font font").innerText;
+    let dateMonth = document.querySelector("[data-ui-role='ui-element'] #date-00000052-month font font").innerText;
+    let dateYear = document.querySelector("[data-ui-role='ui-element'] #date-00000052-year font font").innerText;
+    document.querySelector("blockquote h3").innerHTML = "FETCHA:" + dateDay +'/'+ dateMonth +'/'+ dateYear;
+    
+    let folionum = loader.engine.document.getElementById(108817849).getProperty('value.value');
+    document.querySelector("blockquote h4").innerHTML = "FOLIO: #" + folionum;
+    
     let hotelname = loader.engine.document.getElementById(108818319).getProperty('value.value');
-    document.querySelector("blockquote h5").innerHTML = hotelname;
-    let folionum = loader.engine.document.getElementById(108817871).getProperty('value.value');
-    document.querySelector("blockquote h4").innerHTML = folionum;
+    document.querySelector("blockquote h5").innerHTML = "CLIENT:" + hotelname;
+    
 const joinedValues = valueOne + ": " + valueTwo + " - " + valueThree ;
 		   loader.engine.document.getElementById(108821728).setValue(({"value": joinedValues}));
 }, 60);
@@ -14,5 +22,4 @@ const joinedValues = valueOne + ": " + valueTwo + " - " + valueThree ;
 
 window.onchange = joinValues;
 window.onload = joinValues;
-
 
