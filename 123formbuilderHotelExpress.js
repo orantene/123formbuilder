34f9d8bd35f331b34f9012635bb83a9f9368ef28
orantene <orantene@gmail.com>
2023-08-15 04:51:30 -0500

    Update 123formbuilderHotelExpress.js

--- 123formbuilderHotelExpress.js
+++ 123formbuilderHotelExpress.js
@@ -2,6 +2,7 @@ function joinValues() {
 setTimeout(function() {
 	var valueOne = loader.engine.document.getElementById(108817849).getProperty('value.value');
 	var valueTwo = loader.engine.document.getElementById(108817871).getProperty('value.value');
+	var valueThree = loader.engine.document.getElementById(108818319).getProperty('value.value');
 
 const joinedValues = valueOne + ": " + valueTwo + " - " + valueThree ;
 		   loader.engine.document.getElementById(108821728).setValue(({"value": joinedValues}));
